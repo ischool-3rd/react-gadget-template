bd9794cb7c133e40ef0b432490ad56d053a2ba31
fengpu-ischool <feng.pu@ischool.com.tw>
2022-04-13 11:02:20 +0800

    uds 設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,43 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+
 
 function App() {
+  // 呼叫 Service
+  var _connection:any = null;
+  // 判斷傳入參數，是家長或學生，依身分呼叫不同 Contract。
+  if (window.gadget._system_position === "parent") {
+    _connection = window.gadget.getContract("ischool.exam.parent");
+  } else {
+    _connection = window.gadget.getContract("ischool.exam.student");
+  }
+  // 讀取學生資訊
+  async function GetStudentInfo() {
+    await _connection.send({
+      service: "_.GetStudentInfo",
+      body: {},
+      result: function (response: any, error: any, http: any) {
+        if (error !== null) {
+          console.log('ranks error std ', error);
+          return 'err';
+        } else {
+          if (response) {
+
+            console.log('ranks std ', response);
+
+
+
+          }
+        }
+      }
+    });
+  }
+  //測試取用學生資訊
+  useEffect(()=>{
+    GetStudentInfo();//TODO: replace this with services you needed
+  },[]);
   return (
     <div className="App">
       <header className="App-header">
